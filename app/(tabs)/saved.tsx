0a1f19570d01c6e1317981b5ddd13197a667fb1c
sjoerdwol <sjoerd@woltersonline.net>
2025-03-11 20:24:33 +0100

    implemented basic navigation and changed app icon

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function SavedPage() {
+  return (
+    <View>
+      <Text>saved</Text>
+    </View>
+  )
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,18 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./globals.css"
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="(tabs)"
+        />
+        <Stack.Screen
+          name="movies/[id]"
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }

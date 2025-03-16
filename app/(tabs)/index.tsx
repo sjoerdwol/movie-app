15ad6c9543926b885ff065b9d836d05e15ff3833
sjoerdwol <sjoerd@woltersonline.net>
2025-03-16 14:46:19 +0100

    implemted movie card rendering on start page according to api fetch status

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,23 @@
 //react-native
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 //expo
 import { useRouter } from "expo-router";
 
+//service calls
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+
 //custom components, images etc.
+import MovieCard from "@/components/movieCard";
 import SearchBar from "@/components/searchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
 export default function Index() {
   const router = useRouter();
+
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -23,15 +30,48 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}
       >
-        <Image
-          source={icons.logo}
-          className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className="flex-1 mt-5 ">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color='#0000ff'
+            className="mt-10 self-center"
           />
-        </View>
+        ) : moviesError ? (
+          <Text>Error: {moviesError.message}</Text>
+        ) : (
+          <View>
+            <Image
+              source={icons.logo}
+              className="w-12 h-10 mt-20 mb-5 mx-auto" />
+            <View className="flex-1 mt-5 ">
+              <SearchBar
+                onPress={() => router.push("/search")}
+                placeholder="Search for a movie"
+              />
+              <>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+                <FlatList
+                  data={movies}
+                  keyExtractor={(item) => item.id}
+                  renderItem={({ item }) => (
+                    <MovieCard
+                      {...item}
+                    />
+                  )}
+                  numColumns={3}
+                  columnWrapperStyle={{
+                    justifyContent: 'center',
+                    gap: 20,
+                    paddingHorizontal: 5,
+                    marginBottom: 10
+                  }}
+                  className="mt-2 pb-32"
+                  scrollEnabled={false}
+                />
+              </>
+            </View>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
---
+++ components/movieCard.tsx
@@ -0,0 +1,38 @@
+//react native
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+//expo
+import { Link } from 'expo-router';
+
+//custom icons
+import { icons } from '@/constants/icons';
+
+export default function MovieCard({ id, poster_path, title, vote_average, release_date }: Movie) {
+  return (
+    <Link href={`/movies/${id}`} asChild>
+      <TouchableOpacity className='w-[30%]'>
+        <Image
+          source={{
+            uri: poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
+          }}
+          className='w-full h-52 rounded-lg'
+          resizeMode='cover'
+        />
+        <Text className='text-sm font-bold text-white mt-2' numberOfLines={1}>{title}</Text>
+        <View className='flex-row items-center justify-start gap-x-1'>
+          <Image
+            source={icons.star}
+            className='size-4'
+          />
+
+          <Text className='text-white text-sm'>{Math.round(vote_average)}</Text>
+        </View>
+        <View className='flex-row items-center justify-between'>
+          <Text className='text-sm text-light-300 font-medium mt-1'>{release_date.split('-')[0]}</Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
+  )
+}

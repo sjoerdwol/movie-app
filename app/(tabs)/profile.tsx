dd33b2abfa0db7871124f96dfa3621c77d3ee4b5
sjoerdwol <sjoerd@woltersonline.net>
2025-04-06 22:00:56 +0200

    placeholders for watchlist and profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native'
+// react native
+import { View, Text, Image } from 'react-native'
+
+//custom components, images etc.
+import { icons } from '@/constants/icons'
 
 export default function ProfilePage() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className='bg-primary flex-1 px-10'>
+      <View className='flex justify-center items-center flex-1 flex-col gap-5'>
+        <Image
+          source={icons.person}
+          className='size-10'
+          tintColor='#fff'
+        />
+        <Text className='text-gray-500 text-base'>Profile</Text>
+      </View>
     </View>
   )
 }
